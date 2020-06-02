a1140eb9e2fd400ae3b0731544297d2edb64dec5
Prateek <codes.prateek@gmail.com>
2020-06-02 18:25:49 +0530

    Initial Commit

---
+++ app/src/main/java/com/androidmonk/firebasestorage/CloudStorageActivity.java
@@ -0,0 +1,14 @@
+package com.androidmonk.firebasestorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CloudStorageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cloud_storage);
+    }
+}
